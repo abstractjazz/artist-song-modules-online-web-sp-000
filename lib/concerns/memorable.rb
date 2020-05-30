79401feec9df45cb2b5d3416d3413e43d9c8eca2
Evan Burton <evan@heyevan.com>
2020-05-30 21:56:58 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,6 +1,6 @@
 module Memorable
 module ClassMethods
-  
+
   def reset_all
     self.all.clear
   end
@@ -8,4 +8,10 @@ module ClassMethods
   def count
     self.all.count
   end
-end
+
+module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end 
